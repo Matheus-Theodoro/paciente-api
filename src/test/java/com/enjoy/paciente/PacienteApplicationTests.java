aaeefe4f83b709082769a13fb6e076a8e66eb4a3
Matheus-Theodoro <teteucorreia2009@gmail.com>
2021-12-20 17:00:14 -0300

    commit inicial

---
+++ src/test/java/com/enjoy/paciente/PacienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enjoy.paciente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
